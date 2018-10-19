2891104b707f5e30e2b07a4744c6e9a3b95ddfff
architectj <architectj@gmail.com>
2018-10-20 05:19:57 +0900

    테스트 커밋

---
+++ running/src/main/java/me/hoesoo/runninginfo/RunninginfoApplication.java
@@ -0,0 +1,12 @@
+package me.hoesoo.runninginfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunninginfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunninginfoApplication.class, args);
+	}
+}
